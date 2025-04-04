9162888eced52e2134e4535652c05232218b1d93
Bill Winkler <bill.winkler@gmail.com>
2025-04-04 08:06:55 -0500

    removed debugging printlns

--- src/workshop_api/core.clj
+++ src/workshop_api/core.clj
@@ -15,77 +15,85 @@
 (defn generate-id [] (str (java.util.UUID/randomUUID)))
 (defn current-timestamp [] (Timestamp/from (Instant/now)))
 
-;; Validation
+(defn keywordize-keys [m]
+  (into {} (map (fn [[k v]] [(if (string? k) (keyword k) k) v]) m)))
+
 (defn valid-location? [loc]
   (and (string? (:name loc))
        (string? (:type loc))
        (string? (:area loc))
        (or (nil? (:description loc)) (string? (:description loc)))
-       (or (nil? (:parent-id loc)) (string? (:parent-id loc)))))
+       (or (nil? (:parent_id loc)) (string? (:parent_id loc)))))
 
 (defn valid-item? [item]
-  (and (string? (:name item))
-       (string? (:description item))
-       (string? (:location-id item))
-       (or (nil? (:category item)) (string? (:category item)))
-       (or (nil? (:supplier item)) (string? (:supplier item)))
-       (or (nil? (:quantity item)) (integer? (:quantity item)))
-       (or (nil? (:notes item)) (string? (:notes item)))
-       (or (nil? (:acquisition-date item)) (string? (:acquisition-date item)))))
+  (let [name-ok (string? (:name item))
+        desc-ok (string? (:description item))
+        loc-id-ok (string? (:location_id item))
+        cat-ok (or (nil? (:category item)) (string? (:category item)))
+        sup-ok (or (nil? (:supplier item)) (string? (:supplier item)))
+        qty-ok (or (nil? (:quantity item)) (integer? (:quantity item)))
+        notes-ok (or (nil? (:notes item)) (string? (:notes item)))
+        acq-date-ok (or (nil? (:acquisition_date item)) (string? (:acquisition_date item)))
+        result (and name-ok desc-ok loc-id-ok cat-ok sup-ok qty-ok notes-ok acq-date-ok)]
+    result))
 
-;; Prepare entities
 (defn prepare-location [loc]
   (let [now (current-timestamp)]
     (-> loc
         (assoc :id (generate-id))
-        (assoc :created-at now)
-        (assoc :updated-at now))))
+        (assoc :created_at now)  ;; Changed to underscore
+        (assoc :updated_at now))))  ;; Changed to underscore
 
 (defn prepare-item [item]
   (let [now (current-timestamp)]
     (-> item
         (assoc :id (generate-id))
         (assoc :quantity (or (:quantity item) 1))
-        (assoc :created-at now)
-        (assoc :updated-at now))))
+        (assoc :created_at now)  ;; Changed to underscore
+        (assoc :updated_at now))))  ;; Changed to underscore
 
-;; Database operations
 (defn db-add-location [loc]
   (jdbc/execute-one! ds
                      ["INSERT INTO locations (id, name, description, type, parent_id, area, created_at, updated_at)
-                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)"  ;; Fixed typo: removed extra ?, ?
-                      (:id loc) (:name loc) (:description loc) (:type loc) (:parent-id loc) (:area loc) (:created-at loc) (:updated-at loc)]
+                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+                      (:id loc) (:name loc) (:description loc) (:type loc) (:parent_id loc) (:area loc) (:created_at loc) (:updated_at loc)]
                      {:return-keys true}))
 
 (defn db-add-item [item]
   (jdbc/execute-one! ds
                      ["INSERT INTO items (id, name, category, supplier, description, notes, quantity, location_id, acquisition_date, created_at, updated_at)
                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-                      (:id item) (:name item) (:category item) (:supplier item) (:description item) (:notes item) (:quantity item) (:location-id item) (:acquisition-date item) (:created-at item) (:updated-at item)]
+                      (:id item) (:name item) (:category item) (:supplier item) (:description item) (:notes item) (:quantity item) (:location_id item) (:acquisition_date item) (:created_at item) (:updated_at item)]
                      {:return-keys true}))
 
 (defn db-get-location [id]
-  (jdbc/execute-one! ds ["SELECT * FROM locations WHERE id = ?" id] {:builder-fn rs/as-unqualified-lower-maps}))
+  (jdbc/execute-one! ds
+                     ["SELECT * FROM locations WHERE id = ?" id]
+                     {:builder-fn rs/as-unqualified-lower-maps}))
 
 (defn db-get-items-by-location [location-id]
-  (jdbc/execute! ds ["SELECT * FROM items WHERE location_id = ?" location-id] {:builder-fn rs/as-unqualified-lower-maps}))
+  (jdbc/execute! ds
+                 ["SELECT * FROM items WHERE location_id = ?" location-id]
+                 {:builder-fn rs/as-unqualified-lower-maps}))
 
-;; Handlers
 (defn add-location [request]
-  (let [loc (:body request)]
+  (let [loc (keywordize-keys (:body request))]
     (if (valid-location? loc)
       (let [new-loc (prepare-location loc)]
         (db-add-location new-loc)
         (response {:status "success" :location new-loc}))
-      (status (response {:error "Invalid location format"}) 400))))
+      (status (response {:error "Invalid location format" :data loc}) 400))))
 
 (defn add-item [request]
-  (let [item (:body request)]
+  (let [item (keywordize-keys (:body request))]
     (if (valid-item? item)
-      (let [new-item (prepare-item item)]
-        (db-add-item new-item)
-        (response {:status "success" :item new-item}))
-      (status (response {:error "Invalid item format"}) 400))))
+      (try
+        (let [new-item (prepare-item item)]
+          (db-add-item new-item)
+          (response {:status "success" :item new-item}))
+        (catch Exception e
+          (status (response {:error "Database error" :message (.getMessage e)}) 400)))
+      (status (response {:error "Invalid item format" :data item}) 400))))
 
 (defn get-location-details [id]
   (if-let [loc (db-get-location id)]
@@ -93,13 +101,7 @@
       (response {:location loc :items items}))
     (status (response {:error "Location not found"}) 404)))
 
-;; Simple hello endpoint
-(defn hello []
-  (response {:message "Hello, Workshop API!"}))
-
-;; Routes
 (defroutes app-routes
-  (GET "/hello" [] (hello))  ;; Added this route
   (POST "/location" request (add-location request))
   (POST "/item" request (add-item request))
   (GET "/inventory/location/:id" [id] (get-location-details id)))
