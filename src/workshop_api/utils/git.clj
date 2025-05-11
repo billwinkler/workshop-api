2c637ceea2b6e6d6cc7dc57a5035fbf6fe1cdd93
Bill Winkler <bill.winkler@gmail.com>
2025-05-11 06:32:26 -0500

    debug

--- src/workshop_api/utils/git.clj
+++ src/workshop_api/utils/git.clj
@@ -21,6 +21,7 @@
 (def repo (some #(when (exists? %) %) repos))
 (log/debug "repo:" repo)
 (log/debug "pwd:" (sh "pwd" :dir repo))
+(log/debug "git:" (sh "git" "describe" "--tags" :dir repo))
 
 (defn git-commit-hash []
   (try
